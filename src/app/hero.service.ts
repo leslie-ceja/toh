9283c59b8f543f44539d508b84be5e1a78162ebb
Leslie Ceja <cejaleslie@gmail.com>
2021-06-02 00:27:55 -0500

    imported observable and of

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -3,6 +3,8 @@ import { Injectable } from '@angular/core';
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 
+import { Observable, of } from 'rxjs';
+
 @Injectable({
   providedIn: 'root'
 })
